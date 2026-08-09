3e8d30a441739309e2ce2a353324b25edb15b92d
امیر <139617633+kojamori@users.noreply.github.com>
2026-08-09 11:04:21 +0930

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# NoRocketColliders
+
+A mod for the video game Spaceflight Simulator (SFS) that disables colliders between rockets and other rockets.
+
+![gif](./assets/NoRocketColliders.gif)
+
+Keybind to enable/disable colliders: Keypad Asterisk (Keypad 8 + Leftshift)
+
+## Requirements and Dependencies
+
+- SFS (PC Version)
+  - v1.6.00.16 and newer
+
+## Installation
+
+[Code Mod Installation Guide | SFS Modding Guide](https://kojamori.github.io/SFS-Modding-Guide/getting-started/installation/code-mods)
+
+## Social Media
+
+### Forum Post
+
+TBD
+
+### SFS Modding Guide
+
+https://kojamori.github.io/SFS-Modding-Guide/
+
+### Discord Server
+
+[Join the discord server here!](https://discord.gg/QHEmcehAe9)
+
+# License
+
+See the [LICENSE](LICENSE) file for details.
---
+++ src/Main.cs
@@ -0,0 +1,87 @@
+using HarmonyLib;
+using ModLoader;
+using ModLoader.Helpers;
+using SFS;
+using SFS.Input;
+using SFS.Parts;
+using SFS.UI;
+using System.Collections.Generic;
+using UITools;
+using UnityEngine;
+using static SFS.Input.KeybindingsPC;
+
+namespace NoRocketColliders
+{
+    public class Main : Mod
+    {
+        public static Main Instance { get; private set; }
+        public Main()
+        {
+            Instance = this;
+        }
+
+        public override string ModNameID => "NoRocketColliders";
+        public override string DisplayName => "NoRocketColliders";
+        public override string Author => "kojamori";
+        public override string MinimumGameVersionNecessary => "1.6.00.16";
+        public override string ModVersion => "1.0.0";
+        public override string Description => "A mod that disables colliders between rockets and other rockets.";
+        public override string IconLink => "";
+
+        public override Dictionary<string, string> Dependencies => new Dictionary<string, string>();
+
+        private Harmony _patcher;
+
+        public override void Early_Load()
+        {
+            _patcher = new Harmony(Instance.ModNameID);
+            _patcher.PatchAll();
+        }
+
+        private bool rocketCollisionsDisabled = false;
+
+        private void onWorldSceneLoaded() {
+            ModKeybindings.AddOnKeyDown_World(new CustomKey() { key = KeyCode.Keypad8, shift = true }, () =>
+            {
+                rocketCollisionsDisabled = !rocketCollisionsDisabled;
+                ApplyColliderSettings();
+
+                if (rocketCollisionsDisabled) 
+                {
+                    MsgDrawer.main.Log("Disabled collisions between rockets");
+                }
+                else 
+                {
+                    MsgDrawer.main.Log("Enabled collisions between rockets");
+                }
+            });
+        }
+
+        public override void Load()
+        {
+            // Apply initial collider settings
+            ApplyColliderSettings();
+
+            // Add keybind to toggle collider disabling in World
+            // Keypad asterisk (?)
+            SceneHelper.OnWorldSceneLoaded += onWorldSceneLoaded;
+
+            UnityEngine.Debug.Log("Initialised!");
+        }
+
+        private void ApplyColliderSettings()
+        {
+            int parts = LayerMask.NameToLayer("Parts");
+
+            if (rocketCollisionsDisabled)
+            {
+                Physics2D.IgnoreLayerCollision(parts, parts, true);
+            }
+            else
+            {
+                Physics2D.IgnoreLayerCollision(parts, parts, false);
+            }
+        }
+
+    }
+}
