f71fa541743874c30ac2503e4df5c67dd2ac58fc
امیر <139617633+kojamori@users.noreply.github.com>
2026-08-12 08:45:51 +0930

    add missing src file and fix keybind_example

---
+++ src/ColliderToggle.cs
@@ -0,0 +1,37 @@
+﻿using SFS.UI;
+using UnityEngine;
+
+namespace NoRocketColliders;
+
+public static class ColliderToggle
+{
+    private static bool rocketCollisionsDisabled = false;
+    public static void ApplyColliderSettings()
+    {
+        int parts = LayerMask.NameToLayer("Parts");
+
+        if (rocketCollisionsDisabled)
+        {
+            Physics2D.IgnoreLayerCollision(parts, parts, true);
+        }
+        else
+        {
+            Physics2D.IgnoreLayerCollision(parts, parts, false);
+        }
+    }
+
+    public static void OnKeyDown()
+    {
+        rocketCollisionsDisabled = !rocketCollisionsDisabled;
+        ApplyColliderSettings();
+
+        if (rocketCollisionsDisabled) 
+        {
+            MsgDrawer.main.Log("Disabled collisions between rockets");
+        }
+        else 
+        {
+            MsgDrawer.main.Log("Enabled collisions between rockets");
+        }
+    }
+}
